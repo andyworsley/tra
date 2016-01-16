b3f0488124bc261f6272ac3ed0b4b74506546aeb
Andy Worsley <andy.worsley@graze.com>
2016-01-16 19:22:07 +0000

    First Doctrine test

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\ProductTwo;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,9 +14,26 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        // save an object
+        $product = new ProductTwo();
+        $product->setName('Foo Two');
+        $product->setPrice(random_int(0,100));
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($product);
+        $em->flush();
+
+        // list the objects
+        $info = "";
+        $repo = $em->getRepository('AppBundle:ProductTwo');
+        $products = $repo->findAll();
+        $productPrices = array_map(function(ProductTwo $product) {
+            return $product->getPrice();
+        }, $products);
+
+        // render
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+            'product_prices' => $productPrices
         ));
     }
 }
